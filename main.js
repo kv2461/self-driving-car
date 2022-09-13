5662001261d5ccd7788dd7e1a36825921a54b6ec
Kevin Vicencio <kevinarnoldcode@gmail.com>
2022-09-13 12:55:51 -0700

    initialized sensors

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ const ctx = canvas.getContext("2d");
 const road = new Road (canvas.width/2, canvas.width*0.9);
 const car = new Car(road.getLaneCenter(1),100,30,50); //x,y,width,height
 
-car.draw(ctx);
 
 animate();
 
